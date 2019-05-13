d5ff084369a8245f1046f45da3622b0f297b909d
Jurandy Soares <jurandy.soares@gmail.com>
2019-05-13 16:19:09 -0300

    Ajustes em <horario.ini>

--- modelo/rst/conf.py
+++ modelo/rst/conf.py
@@ -10,7 +10,7 @@ extensions = [
 ]
 templates_path = ['_templates']
 master_doc = 'index'
-project = 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}'
+project = 'Horário {SIGLA_INSTITUICAO}/{SIGLA_CAMPUS} {ANO}.{PERIODO}'
 copyright = '{ANO}, {COLABORADOR} em colaboração com Jurandy Soares'
 author = 'Jurandy Soares'
 version = '{ANO}.{PERIODO}'
@@ -24,20 +24,20 @@ html_static_path = ['_static']
 html_last_updated_fmt = '%d/%b/%Y'
 html_show_sourcelink = False
 html_search_language = 'pt'
-htmlhelp_basename = 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}'
+htmlhelp_basename = 'Horario-{SIGLA_INSTITUICAO}-{SIGLA_CAMPUS}-{ANO}-{PERIODO}'
 latex_elements = {
 }
 latex_documents = [
-    (master_doc, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}.tex', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}',
+    (master_doc, 'Horario-{SIGLA_INSTITUICAO}-{SIGLA_CAMPUS}-{ANO}-{PERIODO}.tex', 'Horário {SIGLA_INSTITUICAO}/{SIGLA_CAMPUS} {ANO}.{PERIODO}',
      'Jurandy Soares', 'manual'),
 ]
 man_pages = [
-    (master_doc, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}',
+    (master_doc, 'Horario-{SIGLA_INSTITUICAO}-{SIGLA_CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA_INSTITUICAO}/{SIGLA_CAMPUS} {ANO}.{PERIODO}',
      [author], 1)
 ]
 texinfo_documents = [
-    (master_doc, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}',
-     author, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}.',
+    (master_doc, 'Horario-{SIGLA_INSTITUICAO}-{SIGLA_CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA_INSTITUICAO}/{SIGLA_CAMPUS} {ANO}.{PERIODO}',
+     author, 'Horario-{SIGLA_INSTITUICAO}-{SIGLA_CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA_INSTITUICAO}/{SIGLA_CAMPUS} {ANO}.{PERIODO}.',
      'Miscellaneous'),
 ]
 epub_title = project
