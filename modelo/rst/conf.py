89972851b99b8342867db923e66ec210ab9e26c3
Jurandy Soares <jurandysoares@users.noreply.github.com>
2019-05-13 16:05:51 -0300

    Modelo uploaded/submetido

---
+++ modelo/rst/conf.py
@@ -0,0 +1,47 @@
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
+import sys
+import os
+extensions = [
+    'sphinx.ext.mathjax',
+]
+templates_path = ['_templates']
+master_doc = 'index'
+project = 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}'
+copyright = '{ANO}, {COLABORADOR} em colaboração com Jurandy Soares'
+author = 'Jurandy Soares'
+version = '{ANO}.{PERIODO}'
+release = '{ANO}.{PERIODO}'
+language = 'pt_BR'
+exclude_patterns = ['_build']
+pygments_style = 'sphinx'
+todo_include_todos = False
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_last_updated_fmt = '%d/%b/%Y'
+html_show_sourcelink = False
+html_search_language = 'pt'
+htmlhelp_basename = 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}'
+latex_elements = {
+}
+latex_documents = [
+    (master_doc, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}.tex', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}',
+     'Jurandy Soares', 'manual'),
+]
+man_pages = [
+    (master_doc, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}',
+     [author], 1)
+]
+texinfo_documents = [
+    (master_doc, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}',
+     author, 'Horario-{SIGLA-INSTITUICAO}-{SIGLA-CAMPUS}-{ANO}-{PERIODO}', 'Horário {SIGLA-INSTITUICAO}/{SIGLA-CAMPUS} {ANO}.{PERIODO}.',
+     'Miscellaneous'),
+]
+epub_title = project
+epub_author = author
+epub_publisher = author
+epub_copyright = copyright
+epub_exclude_files = ['search.html']
---
+++ modelo/rst/index.rst
@@ -0,0 +1,18 @@
+.. Horário IFRN/PAR 2018.2 documentation master file, created by
+   sphinx-quickstart on Mon Apr  3 07:02:54 2017.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Horário IFRN/PAR 2019.1
+=========================
+
+.. toctree::
+   :maxdepth: 1
+
+   Professores <professor/index.rst>
+   Salas <sala/index.rst>
+   Turmas <turma/index.rst>
+
+
+
+
